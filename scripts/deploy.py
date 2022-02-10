d051a51b388f245cdfa95ad8f8277558db08c5d2
Kirill Sh <kshumov89@gmail.com>
2022-02-10 09:21:10 +0300

    Add read value

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,15 +1,23 @@
 import os
 
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts.add(config["wallets"]["from_key"])
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     transaction = simple_storage.store(33, {"from": account})
     transaction.wait(1)
     updated_store_value = simple_storage.retrieve()
     print(updated_store_value)
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
 
 def main():
     deploy_simple_storage()
